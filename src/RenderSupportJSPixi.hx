9c10b6aac9a188d1fa39306d0b065aa45b6a8d77
Stanislav Podolia <stanislavpodolia@gmail.com>
2019-01-11 18:06:39 +0200

    JS: Fix video sprite _visible property
    JS: Fix exitFullscreen error
    MVideoPlayer: Handle FVideoControls

--- src/RenderSupportJSPixi.hx
+++ src/RenderSupportJSPixi.hx
@@ -2984,14 +2984,16 @@ class RenderSupportJSPixi {
 			element = untyped Browser.document;
 		}
 
-		if (untyped element.exitFullscreen != null)
-			untyped element.exitFullscreen();
-		else if (untyped element.mozCancelFullScreen != null)
-			untyped element.mozCancelFullScreen();
-		else if (untyped element.webkitExitFullscreen != null)
-			untyped element.webkitExitFullscreen();
-		else if (untyped element.msExitFullscreen != null)
-			untyped element.msExitFullscreen();
+		if (untyped element.fullscreenElement != null) {
+			if (untyped element.exitFullscreen != null)
+				untyped element.exitFullscreen();
+			else if (untyped element.mozCancelFullScreen != null)
+				untyped element.mozCancelFullScreen();
+			else if (untyped element.webkitExitFullscreen != null)
+				untyped element.webkitExitFullscreen();
+			else if (untyped element.msExitFullscreen != null)
+				untyped element.msExitFullscreen();
+		}
 	}
 
 	public static function toggleFullScreen(fs : Bool) : Void {
@@ -3385,6 +3387,7 @@ private class VideoClip extends FlowContainer {
 		untyped videoTexture.baseTexture.autoPlay = !startPaused;
 		untyped videoTexture.baseTexture.autoUpdate = false;
 		videoSprite = new Sprite(videoTexture);
+		untyped videoSprite._visible = true;
 		addChild(videoSprite);
 
 		RenderSupportJSPixi.PixiStage.on("drawframe", updateNativeWidget);
