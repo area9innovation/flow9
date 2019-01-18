86af142630a483275b0ca5704c12d1a0e29cfd2d
Dmitry Vlasov <vlasov@academ.org>
2019-01-18 18:39:01 +0700

    flowc: flowc1.bat uses flowc1 to run

--- bin/flowc1.bat
+++ bin/flowc1.bat
@@ -1,8 +1,11 @@
-@echo off
+::@echo off
 
-PATH %PATH%;%JAVA_HOME%\bin\
-for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j"
-set argsFor8=
-if %jver% == 8 (set argsFor8=-XX:+UseConcMarkSweepGC -XX:ParallelCMSThreads=2)
+::PATH %PATH%;%JAVA_HOME%\bin\
+::for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j"
+::set argsFor8=
+::if %jver% == 8 (set argsFor8=-XX:+UseConcMarkSweepGC -XX:ParallelCMSThreads=2)
 
-java -Xss32m -Xms256m -Xmx4G %argsFor8% -cp src/java/;. -jar %~dp0..\tools\flowc\flowc.jar bin-dir=%~dp0 %*
+::java -Xss32m -Xms256m -Xmx4G %argsFor8% -cp src/java/;. -jar %~dp0..\tools\flowc\flowc.jar bin-dir=%~dp0 %*
+
+:: Use python script to run flowc1
+python flowc1 %*
