25b72172fe6dd157215bedb0f2133ff0529fa342
Pavel Zherdin <pavel@area9.dk>
2019-01-31 15:06:12 +0100

    Enable old mysql workaround in qt container
    Attempted to disable it before but turns out that bug with opening and
    closing connections is still present.

--- QtByteRunner/docker/qt/build.sh
+++ QtByteRunner/docker/qt/build.sh
@@ -10,16 +10,16 @@ echo "When switching between QT versions, you have to manually clean up download
 # - Some libraries may become deprecated and get removed
 # - libmysqlclient may get fixed rendering manual mysql driver compiling obsolete
 QT_VERSION=5.12.0
-#MYSQL_TAR=mysql-5.6.36-linux-glibc2.5-x86_64.tar.gz
+MYSQL_TAR=mysql-5.6.36-linux-glibc2.5-x86_64.tar.gz
 
-#DOWNLOADS=downloads
-#mkdir -p $DOWNLOADS
-#pushd $DOWNLOADS
+DOWNLOADS=downloads
+mkdir -p $DOWNLOADS
+pushd $DOWNLOADS
 
-#if [ ! -f $MYSQL_TAR ]; then
-#  wget https://dev.mysql.com/get/Downloads/MySQL-5.6/$MYSQL_TAR
-#fi
-#popd
+if [ ! -f $MYSQL_TAR ]; then
+  wget https://dev.mysql.com/get/Downloads/MySQL-5.6/$MYSQL_TAR
+fi
+popd
 
 sed "s|%QT_VERSION%|${QT_VERSION}|" Dockerfile.build.template > Dockerfile.build
 docker build \
--- QtByteRunner/docker/qt/config/build_mysql_driver.sh
+++ QtByteRunner/docker/qt/config/build_mysql_driver.sh
@@ -19,14 +19,19 @@ make
 # want to learn it, I just want a statically compiled driver.
 # So abusing the fact that make above has created mysql/Makefile and replacing
 # the libraries in it manually.
+
 # This is done because there is a bug in recent versions of mysqlclient that 
-# causes segfault on repeated connections to the database.
-#tar xzf mysql.tar.gz
-#cp mysql-5.6.36-linux-glibc2.5-x86_64/lib/libmysqlclient.a mysql/
-#rm -rf mysql-5.6.36-linux-glibc2.5-x86_64
-#rm mysql.tar.gz
+# causes segfault on repeated connections to the database. It is still present in ubuntu 18.
+tar xzf mysql.tar.gz
+cp mysql-5.6.36-linux-glibc2.5-x86_64/lib/libmysqlclient.a mysql/
+rm -rf mysql-5.6.36-linux-glibc2.5-x86_64
+rm mysql.tar.gz
+
+# comment above and uncomment below to try os provided sql
 # taking os provided static mysql lib
-cp /usr/lib/x86_64-linux-gnu/libmysqlclient.a mysql/
+#cp /usr/lib/x86_64-linux-gnu/libmysqlclient.a mysql/
+# There's no test case, just try to open and close DB connection twice in succession.
+
 sed -i 's/-lmysqlclient /libmysqlclient.a -ldl -lpthread -lz/' mysql/Makefile
 pushd mysql
 make clean
