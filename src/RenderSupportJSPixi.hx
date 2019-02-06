76d48241101b1014a2e102525f3c7486a38fbe89
Ivan Vereschaga <vereschaga.ivan@gmail.com>
2019-02-06 17:58:36 +0200

    iOS: Attempt to fix resize problem of flowjs into iframe

--- src/RenderSupportJSPixi.hx
+++ src/RenderSupportJSPixi.hx
@@ -887,8 +887,8 @@ class RenderSupportJSPixi {
 		if (backingStoreRatio != PixiRenderer.resolution) {
 			createPixiRenderer();
 		} else {
-			var win_width = e.target.innerWidth + 1;
-			var win_height = e.target.innerHeight + 1;
+			var win_width = Browser.window.innerWidth + 1;
+			var win_height = Browser.window.innerHeight + 1;
 
 			if (Platform.isAndroid || (Platform.isIOS && Platform.isChrome)) {
 				// Still send whole window size - without reducing by screen kbd
