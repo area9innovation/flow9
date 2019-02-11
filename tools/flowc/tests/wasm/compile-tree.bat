f84678ffb81b90114fe8807e8bf436b45d5dafa1
Vadim Lisichenkov <vadlis@yandex.ru>
2019-02-12 01:51:42 +0300

    wasm: attempt to get test44 to work. (#87)
    * wasm: attempt to get test44 to work.
    
    * fix explicit cast
    
    *  - added switch debugging
    
    * add cast test (47)

--- tools/flowc/tests/wasm/compile-tree.bat
+++ tools/flowc/tests/wasm/compile-tree.bat
@@ -1,5 +1,5 @@
 @echo off
 REM This batch will compile flow file and prints AST to console.
 REM All outputs performs to appropriate [name]_out.txt file
-call flowc server=0 file=%1.flow wasm=%1 wasmhost=%1.js wasmast=%1_out.txt
+call flowc server=0 incremental=0 verify-types=1 file=%1.flow wasm=%1 wasmhost=%1.js wasmast=%1_out.txt
 echo Done
