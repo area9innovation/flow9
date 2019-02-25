05b7b4900b8ae5453d7ce00ea5a0d78cbd1e7ffc
Dmitry Vlasov <vlasov@academ.org>
2019-02-25 18:01:29 +0700

    kate: flow plugin is updated

--- resources/kate/flow/CompilerTextEdit.cpp
+++ resources/kate/flow/CompilerTextEdit.cpp
@@ -15,12 +15,12 @@ void CompilerTextEdit::mousePressEvent(QMouseEvent *e) {
 	QTextCursor cursor = cursorForPosition(e->pos());
 	cursor.select(QTextCursor::LineUnderCursor);
 	QString line = cursor.selectedText();
-	static QRegExp errorPosExp(QLatin1String("^(([A-Za-z]:/)?[^:]*):([0-9]*):(([0-9]*):?)?.*"));
+	static QRegExp errorPosExp(QLatin1String("^(([A-Za-z]:/)?[^:]*):([0-9]*)(:([0-9]*):?)?.*"));
 	if (errorPosExp.exactMatch(line)) {
 		QString file = errorPosExp.cap(1);
 		int line = errorPosExp.cap(3).toInt();
 		int col = errorPosExp.cap(5).toInt();
-		emit signalCompilerError(file, line - 1, col);
+		emit signalCompilerError(file, line, col);
 	}
 	QPlainTextEdit::mousePressEvent(e);
 }
