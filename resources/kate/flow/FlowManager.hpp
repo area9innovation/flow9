4aa67540bc43d58015905f4a165fbf555d77e242
Dmitry Vlasov <vlasov@academ.org>
2019-03-25 15:33:24 +0700

    flowc: Kate plugin: aux compiler calls don't spoil console output

--- resources/kate/flow/FlowManager.hpp
+++ resources/kate/flow/FlowManager.hpp
@@ -83,6 +83,43 @@ private:
 			auto time = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
 			return static_cast<double>(time) / 1000.0;
 		}
+		bool showCompilerOutput() const {
+			switch (state) {
+			case IDLE:        return false;
+			case COMPILING:   return true;
+			case RUNNING:     return true;
+			case BUILDING:    return true;
+			case DEBUGGING:   return true;
+			case LOOKUP_DEF:  return false;
+			case LOOKUP_TYPE: return false;
+			case LOOKUP_USES: return true;
+			case RENAMING:    return false;
+			case DUMPING_IDS: return false;
+			case OUTLINE:     return false;
+			default:          return true;
+			};
+		}
+		QString show(bool full = false) const {
+			QString ret;
+			switch (state) {
+			case IDLE:        ret += QLatin1String("IDLE");        break;
+			case COMPILING:   ret += QLatin1String("COMPILING");   break;
+			case RUNNING:     ret += QLatin1String("RUNNING");     break;
+			case BUILDING:    ret += QLatin1String("BUILDING");    break;
+			case DEBUGGING:   ret += QLatin1String("DEBUGGING");   break;
+			case LOOKUP_DEF:  ret += QLatin1String("LOOKUP_DEF");  break;
+			case LOOKUP_TYPE: ret += QLatin1String("LOOKUP_TYPE"); break;
+			case LOOKUP_USES: ret += QLatin1String("LOOKUP_USES"); break;
+			case RENAMING:    ret += QLatin1String("RENAMING");    break;
+			case DUMPING_IDS: ret += QLatin1String("DUMPING_IDS"); break;
+			case OUTLINE:     ret += QLatin1String("OUTLINE"); break;
+			};
+			if (full) {
+				ret += QLatin1Char('\n');
+				ret += QLatin1String("output:\n") + output + QLatin1String("\n");
+			}
+			return ret;
+		}
 		State state;
 		QVariant data;
 		QString output;
@@ -93,6 +130,9 @@ private:
 	struct StateKeeper {
 		InternalState get() {
 			internal_state_.end = std::chrono::high_resolution_clock::now();
+			return internal_state_;
+		}
+		InternalState peek() {
 			return internal_state_;
 		}
 		QString& output() { return internal_state_.output; }
