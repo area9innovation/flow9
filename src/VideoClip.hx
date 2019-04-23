368b0a7819dc8a81829e1f5c21b5e85b4b2bb9b7
Pavel Zotov <pavel.v.zotov@gmail.com>
2019-04-23 11:39:21 +0500

    && -> ||

--- src/VideoClip.hx
+++ src/VideoClip.hx
@@ -129,7 +129,7 @@ class VideoClip extends FlowContainer {
 		createStreamStatusListeners();
 		createFullScreenListeners();
 
-		if (!startPaused && !CanAutoPlay)
+		if (!startPaused || !CanAutoPlay)
 			playFn(false);
 	}
 
