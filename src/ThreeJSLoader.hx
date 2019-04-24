ee82dbc178dd7c5d951d87d517d775f8a48f2c29
Stanislav Podolia <stanislavpodolia@gmail.com>
2019-04-24 18:51:54 +0300

    Add T3DStage, M3DStage

--- src/ThreeJSLoader.hx
+++ src/ThreeJSLoader.hx
@@ -1,32 +1,7 @@
 import js.Browser;
-// import pixi.core.display.Container;
-// import pixi.core.display.DisplayObject;
-// import pixi.core.math.shapes.Rectangle;
-// import pixi.core.math.Point;
-
-// import js.three.Camera;
-// import js.three.Scene;
-// import js.three.Object3D;
-// import js.three.WebGLRenderer;
-
-// using DisplayObjectHelper;
 
 class ThreeJSLoader {
 	public function new(objUrl : String, mtlUrl : String, onLoad : Dynamic -> Void) {
-		// var ext = url.substr(url.length - 3);
-
-		// trace(ext);
-
-		// if (ext == "mtl") {
-		// 	untyped __js__("new THREE.MTLLoader().load(url, function (materials) {
-		// 		materials.preload();
-
-		// 		onLoad(materials);
-		// 	});");
-		// } else if (ext == "obj") {
-		// 	untyped __js__("new THREE.OBJLoader().setMaterial(materials).load(url, onLoad);");
-		// }
-
 		untyped __js__("
 			new MTLLoader()
 				.load(mtlUrl, function (materials) {
