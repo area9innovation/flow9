f0cf7acd5a12da7e0240309ccb5ed66e5b826f6d
Ivan Vereschaga <vereschaga.ivan@gmail.com>
2019-05-29 14:38:53 +0300

    JS: Implement message event listener

--- platforms/js/RenderSupportJSPixi.hx
+++ platforms/js/RenderSupportJSPixi.hx
@@ -254,6 +254,7 @@ class RenderSupportJSPixi {
 		preventDefaultFileDrop();
 		initPixiStageEventListeners();
 		initBrowserWindowEventListeners();
+		initMessageListener();
 		initFullScreenEventListeners();
 		FontLoader.loadWebFonts(StartFlowMain);
 		initClipboardListeners();
@@ -280,7 +281,6 @@ class RenderSupportJSPixi {
 	private static inline function initBrowserWindowEventListeners() {
 		WindowTopHeight = cast (getScreenSize().height - Browser.window.innerHeight);
 		Browser.window.addEventListener('resize', onBrowserWindowResize, false);
-		Browser.window.addEventListener('message', receiveWindowMessage); // Messages from crossdomaid iframes
 		Browser.window.addEventListener('focus', function () { PixiStage.invalidateStage(); requestAnimationFrame(); }, false);
 	}
 
@@ -309,6 +309,10 @@ class RenderSupportJSPixi {
 		Browser.document.addEventListener('paste', handler, false);
 	}
 
+	private static inline function initMessageListener() {
+		Browser.document.addEventListener('message', receiveWindowMessage, false);
+	}
+
 	private static inline function initFullScreenEventListeners() {
 		for (e in ['fullscreenchange', 'mozfullscreenchange', 'webkitfullscreenchange', 'MSFullscreenChange']) {
 			Browser.document.addEventListener(e, fullScreenTrigger, false);
@@ -316,6 +320,8 @@ class RenderSupportJSPixi {
 	}
 
 	private static function receiveWindowMessage(e : Dynamic) {
+		emit("message", e.data);
+
 		var hasNestedWindow : Dynamic = null;
 		hasNestedWindow = function(iframe : IFrameElement, win : js.html.Window) {
 			try {
@@ -781,6 +787,11 @@ class RenderSupportJSPixi {
 	public static function addPasteEventListener(fn : Array<Dynamic> -> Void) : Void -> Void {
 		on("paste", fn);
 		return function() { off("paste", fn); };
+	}
+
+	public static function addMessageEventListener(fn : String -> Void) : Void -> Void {
+		on("message", fn);
+		return function() { off("message, fn"); };
 	}
 
 	public static inline function InvalidateStage() : Void {
