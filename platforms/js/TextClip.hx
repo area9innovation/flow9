ba476e0e743f83d372e89c28eb4a025eaee8855c
Kirill Bykov <brawaga@gmail.com>
2019-07-09 14:28:47 +0700

    Revert "Workaround due jspixi does not support BiDi settings directly. trello.com/c/HYZUQKQW/5545-text-alignment-in-arabic"
    This reverts commit 53023e06

--- platforms/js/TextClip.hx
+++ platforms/js/TextClip.hx
@@ -364,22 +364,14 @@ class TextClip extends NativeWidgetClip {
 		}
 	}
 
-	private static function bidiDecorate(text : String, dir : String) : String {
-		if (dir == 'ltr') {
+	private function bidiDecorate(text : String) : String {
+		if (textDirection == 'ltr') {
 			return String.fromCharCode(0x202A) + text + String.fromCharCode(0x202C);
-		} else if (dir == 'rtl') {
+		} else if (textDirection == 'rtl') {
 			return String.fromCharCode(0x202B) + text + String.fromCharCode(0x202C);
 		} else {
 			return text;
 		}
-	}
-
-	private static function bidiUndecorate(text : String) : Array<String> {
-		if (text.charCodeAt(text.length-1) == 0x202C) {
-			if (text.charCodeAt(0) == 0x202A) return [text.substr(1, text.length-2), 'ltr'];
-			if (text.charCodeAt(0) == 0x202B) return [text.substr(1, text.length-2), 'rtl'];
-		}
-		return [text, ''];
 	}
 
 	private static inline function capitalize(s : String) : String {
@@ -446,7 +438,7 @@ class TextClip extends NativeWidgetClip {
 
 		measureFont();
 
-		this.text = bidiDecorate(StringTools.endsWith(text, '\n') ? text.substring(0, text.length - 1) : text, textDirection);
+		this.text = StringTools.endsWith(text, '\n') ? text.substring(0, text.length - 1) : text;
 		this.backgroundColor = backgroundColor;
 		this.backgroundOpacity = backgroundOpacity;
 
@@ -714,7 +706,6 @@ class TextClip extends NativeWidgetClip {
 		if (this.textDirection != textDirection) {
 			this.textDirection = textDirection.toLowerCase();
 
-			this.text = bidiDecorate(bidiUndecorate(text)[0], textDirection);
 			invalidateStyle();
 		}
 	}
