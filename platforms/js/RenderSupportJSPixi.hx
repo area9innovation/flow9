ccf840c0a9b647719550b8b1b278d29d5ed0a1ee
Artem Hryn <plartem01@gmail.com>
2019-08-28 14:31:04 +0300

    Revert "JS: Don't emit mouseout when mouse is moved form parent to child"
    This reverts commit 500ab41af797f8198c87329025b87f0b0963a4ef.

--- platforms/js/RenderSupportJSPixi.hx
+++ platforms/js/RenderSupportJSPixi.hx
@@ -576,9 +576,7 @@ class RenderSupportJSPixi {
 					}
 
 					// Prevent from mouseout to native clip or droparea element to allow dragging over
-					if (e.toElement.parentNode == e.fromElement ||
-						checkElement(e.toElement) && e.fromElement != null ||
-						checkElement(e.fromElement) && e.toElement != null)
+					if (checkElement(e.toElement) && e.fromElement != null || checkElement(e.fromElement) && e.toElement != null)
 						return;
 
 					listener();
