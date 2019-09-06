c96a8ba0036f1b27155afad767b8d2595c3cd3ab
Dima Bure <dmitry.bure@area9.dk>
2019-09-06 11:10:17 +0200

    Thread safe improvements (#353)
    - introduced a way to run asynchronous requests with callbacks in separate threads
    - implemented wrapper around Java concurrent hash map
    - trying to make all println's thread safe in Java

--- platforms/java/com/area9innovation/flow/Native.java
+++ platforms/java/com/area9innovation/flow/Native.java
@@ -44,6 +44,7 @@ import java.time.ZoneOffset;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeParseException;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SuppressWarnings("unchecked")
 public class Native extends NativeHost {
@@ -67,8 +68,10 @@ public class Native extends NativeHost {
 		}
 
 		try {
-			PrintStream out = new PrintStream(System.out, true, "UTF-8");
-			out.println(s);
+			synchronized (System.out) {
+				PrintStream out = new PrintStream(System.out, true, "UTF-8");
+				out.println(s);
+			}
 		} catch(UnsupportedEncodingException e) {
 		}
 		return null;
@@ -1267,6 +1270,54 @@ public class Native extends NativeHost {
 	  return resArr;
 	}
 
+	public final Object concurrentAsyncCallback(Func2<Object, String, Func1<Object, Object>> task, Func1<Object,Object> onDone) {
+		// thread #1
+		CompletableFuture.supplyAsync(() -> {
+			// thread #2
+			CompletableFuture<Object> completableFuture = new CompletableFuture<Object>();
+			task.invoke(Long.toString(Thread.currentThread().getId()), (res) -> {
+				// thread #2
+				completableFuture.complete(res);
+				return null;
+			});
+			Object result = null;
+			try {
+				result = completableFuture.get();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+			return result;
+		}, threadpool).thenApply(result -> {
+			// thread #2
+			return onDone.invoke(result);
+		});
+
+		return null;
+	}	
+
+	public final Object initConcurrentHashMap() {
+		return new ConcurrentHashMap();
+	}
+
+	public final Object setConcurrentHashMap(Object map, Object key, Object value) {
+		ConcurrentHashMap concurrentMap = (ConcurrentHashMap) map;
+		concurrentMap.put(key, value);
+		return null;
+	}
+
+	public final Object getConcurrentHashMap(Object map, Object key) {
+		ConcurrentHashMap concurrentMap = (ConcurrentHashMap) map;
+		return concurrentMap.get(key);
+	}
+
+	public final Boolean containsConcurrentHashMap(Object map, Object key) {
+		ConcurrentHashMap concurrentMap = (ConcurrentHashMap) map;
+		return concurrentMap.containsKey(key);
+	}
+
+	// TODO: why don't we use threadpool here?
 	public final Object concurrentAsyncOne(Boolean fine, Func0<Object> task, Func1<Object,Object> callback) {
 		CompletableFuture.supplyAsync(() -> {
 			return task.invoke();
