282e19c6b0dfbe87e907a0c7b8242a5a6db97843
dmitry-vlasov <vlasov@academ.org>
2019-10-04 21:39:43 +0700

    Revert "flowc: windows bat scripts are fixed"
    This reverts commit 7a0b7ce818532974517201938d3aa682deb7fe1b.

--- bin/flowc1.bat
+++ bin/flowc1.bat
@@ -14,24 +14,31 @@ if "%Located%"=="" goto else
 rem if JDK located display message to user
 rem update %JAVA_HOME%
 set JAVA_HOME=%Located%
+echo     JAVA_HOME has been set to:
+echo         %JAVA_HOME%
 goto endif
 
 :else
 rem if JDK was not located
 rem if %JAVA_HOME% has been defined then use the existing value
 if "%JAVA_HOME%"=="" goto NoExistingJavaHome
+echo     Existing value of JAVA_HOME will be used:
+echo         %JAVA_HOME%
 goto endif
 
 :NoExistingJavaHome
 rem display message to the user that %JAVA_HOME% is not available
 echo     No Existing value of JAVA_HOME is available.
+echo     set JAVA_HOME=path to your jdk root
+echo     and retry
 goto endif
 
 :endif
 
-@echo off
-
+set JAVAC=%JAVA_HOME%\bin\javac
+set JAR=%JAVA_HOME%\bin\jar
 set JAVA=%JAVA_HOME%\bin\java
+
 set BASE_DIR=%~dp0..\
 
 if exist %BASE_DIR%\tools\flowc\flowc.jar (
