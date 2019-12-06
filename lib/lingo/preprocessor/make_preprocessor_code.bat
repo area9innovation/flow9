34ebd988cd9fc19f33c099e2d3ca6a44d56039e6
Cyril Dyba <coolrunner86@gmail.com>
2019-12-06 20:10:16 +0300

    Added basic general-purpose preprocessor

---
+++ lib/lingo/preprocessor/make_preprocessor_code.bat
@@ -0,0 +1,2 @@
+cd ..\..\..
+flowcpp --batch lingo/pegcode/pegcompiler.flow -- file=lib/lingo/preprocessor/preprocessor.lingo out=lib/lingo/preprocessor/preprocessor_pegop.flow
---
+++ lib/lingo/preprocessor/make_preprocessor_code.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ../../..
+flowcpp --batch lingo/pegcode/pegcompiler.flow -- file=lib/lingo/preprocessor/preprocessor.lingo out=lib/lingo/preprocessor/preprocessor_pegop.flow
