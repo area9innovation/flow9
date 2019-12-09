fcb4e3c3b85bfcd6b190d5b7c5d04b59e14c374e
Cyril Dyba <coolrunner86@gmail.com>
2019-12-09 19:56:41 +0300

    Moved preprocessor code out of lingo dir

--- lib/lingo/preprocessor/make_preprocessor_code.bat
+++
@@ -1,2 +0,0 @@
-cd ..\..\..
-flowcpp --batch lingo/pegcode/pegcompiler.flow -- file=lib/lingo/preprocessor/preprocessor.lingo out=lib/lingo/preprocessor/preprocessor_pegop.flow
--- lib/lingo/preprocessor/make_preprocessor_code.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ../../..
-flowcpp --batch lingo/pegcode/pegcompiler.flow -- file=lib/lingo/preprocessor/preprocessor.lingo out=lib/lingo/preprocessor/preprocessor_pegop.flow
