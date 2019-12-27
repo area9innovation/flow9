4cc7e1f19582c523c90ea8518061a85c609373d4
Stanislav Podolia <stanislavpodolia@gmail.com>
2019-12-27 15:57:49 +0200

    Revert flowFontStyles optimization

--- platforms/js/FlowFontStyle.hx
+++ platforms/js/FlowFontStyle.hx
@@ -8,7 +8,7 @@ typedef FontStyle = {
 
 // Singleton used to map flow fonts to css styles
 class FlowFontStyle {
-	private static var flowFontStyles : Dynamic;
+	private static var flowFontStyles : Map<String, Dynamic>;
 
 	public static function fromFlowFonts(names : String) : FontStyle {
 		flowFontStyles = null;
@@ -32,19 +32,19 @@ class FlowFontStyle {
 		return styles;
 	}
 
-	private static var flowFontStylesJSON = haxe.Json.parse(haxe.Resource.getString("fontstyles"));
 
 	public static function fromFlowFont(name : String) : FontStyle {
 		if (flowFontStyles == null) {
 			// Convert all flow font names to lowercase in order avoid case inconsistencies
-			flowFontStyles = {};
+			flowFontStyles = new Map<String, Dynamic>();
+			var styles = haxe.Json.parse(haxe.Resource.getString("fontstyles"));
 
-			for (fontname in Reflect.fields(flowFontStylesJSON)) {
-				Reflect.setField(flowFontStyles, fontname.toLowerCase(), Reflect.field(flowFontStylesJSON, fontname));
+			for (fontname in Reflect.fields(styles)) {
+				flowFontStyles.set(fontname.toLowerCase(), Reflect.field(styles, fontname));
 			}
 		}
 
-		var style : FontStyle = Reflect.field(flowFontStyles, name.toLowerCase());
+		var style : FontStyle = flowFontStyles.get(name.toLowerCase());
 		return (style != null) ? style : {family: name, weight: "", size: 0.0, style: "normal", doNotRemap : false};
 	}
 }
