6bf094a6f66985ff39bf2108c3e714597ea36dc3
Ivan Vereschaga <i.vereschaga@intetics.com>
2020-04-03 14:54:54 +0300

    Revert "CPP: Split image textures & load big images on CPU thread"
    This reverts commit 8acdf979bf8b2307925d45e0cce6df30b6591c7a.

--- platforms/common/cpp/gl-gui/GLPictureClip.h
+++ platforms/common/cpp/gl-gui/GLPictureClip.h
@@ -11,12 +11,11 @@ using namespace std;
 class GLPictureClip : public GLClip
 {
 protected:
-    static int MaxTextureSize;
-
     unicode_string name;
     bool only_download, pending;
 
-    vector<vector<GLTextureBitmap::Ptr>> imageGrid;
+    GLTextureBitmap::Ptr image;
+    vector<vector<GLTextureImage::Ptr>> imageGrid;
 
     // ROOTS:
     StackSlot size_callback;
@@ -30,7 +29,6 @@ protected:
     void renderInner(GLRenderer *renderer, GLDrawSurface *surface, const GLBoundingBox &clip_box);
 
 public:
-    static void setMaxTextureSize(int size);
     GLPictureClip(GLRenderSupport *owner, unicode_string name);
 
     bool isOnlyDownload() { return only_download; }
@@ -45,9 +43,8 @@ public:
     DEFINE_FLOW_NATIVE_OBJECT(GLPictureClip, GLClip);
     
 private:
-    void setTextureGrid(GLTextureBitmap::Ptr image);
+    void splitTextureByMaxSize(GLTextureBitmap::Ptr image, int maxTextureSize);
     vec2 computeImageGridSize();
-    void checkNeedsSplitTexture();
     GLTextureBitmap::Ptr cropTextureBitmap(GLTextureBitmap::Ptr image, vec2 offset, vec2 size);
 };
 
