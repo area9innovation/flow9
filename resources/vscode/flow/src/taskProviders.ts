86245c8ef4c89583d8d4535e0c4e0b0557c76d71
Dmitry Vlasov <vlasov@academ.org>
2020-04-29 18:41:02 +0700

    vscode: js backend is added to build/run tasks

--- resources/vscode/flow/src/taskProviders.ts
+++ resources/vscode/flow/src/taskProviders.ts
@@ -5,6 +5,8 @@ interface FlowdTaskDefinition extends vscode.TaskDefinition {
 	program: string; 
 	backend: string;
 	options?: string[];
+	browser?: string;
+	urlPrefix?: string;
 }
 
 export class BuildTaskProvider implements vscode.TaskProvider {
@@ -22,16 +24,20 @@ export class BuildTaskProvider implements vscode.TaskProvider {
 	public resolveTask(task: vscode.Task): vscode.Task | undefined {
 		const def: FlowdTaskDefinition = <any>task.definition;
 		if (def && def.program && def.backend) {
-			const def: FlowdTaskDefinition = <any>task.definition;
+			let options = def.options? def.options : [];
 			let exec = null;
-			if (def.backend == "jar") {
-				exec = new vscode.ShellExecution("flowc1", ["jar=1", def.program].concat(def.options), { cwd: this.workspaceRoot });
-			} else if (def.backend == "bc") {
-				exec = new vscode.ShellExecution("flowc1", ["bytecode=1", def.program].concat(def.options), { cwd: this.workspaceRoot });
+			if (def.backend.toLowerCase() == "jar") {
+				exec = new vscode.ShellExecution("flowc1", ["jar=1", def.program].concat(options), { cwd: this.workspaceRoot });
+			} else if (def.backend.toLowerCase() == "bc") {
+				exec = new vscode.ShellExecution("flowc1", ["bytecode=1", def.program].concat(options), { cwd: this.workspaceRoot });
+			} else if (def.backend.toLowerCase() == "js") {
+				// call flowc1 js=www2/%1.js %1/%1.flow html=www2/%1.html
+				let name = path.basename(path.join(this.workspaceRoot, def.program), ".flow");
+				exec = new vscode.ShellExecution("flowc1", ["js=www2/" + name + ".js", "html=www2/" + name + ".html", def.program].concat(options), { cwd: this.workspaceRoot });
 			}
 			if (!exec) return undefined; else
-			return new vscode.Task(def, vscode.TaskScope.Workspace, `build ${def.program} ${def.backend} ${def.options.join(' ')}`,
-					BuildTaskProvider.scriptType, exec);
+			return new vscode.Task(def, vscode.TaskScope.Workspace, `build ${def.program} ${def.backend} ${options.join(' ')}`,
+					BuildTaskProvider.scriptType, exec, []);
 		} else {
 			return undefined;
 		}
@@ -53,20 +59,30 @@ export class RunTaskProvider implements vscode.TaskProvider {
 	public resolveTask(task: vscode.Task): vscode.Task | undefined {
 		const def: FlowdTaskDefinition = <any>task.definition;
 		if (def && def.program && def.backend) {
+			let options = def.options? def.options : [];
 			let baseName = path.basename(path.join(this.workspaceRoot, def.program), ".flow");
 			let exec = null;
-			if (def.backend == "jar") {
-				exec = new vscode.ShellExecution("java", ["-jar", baseName + ".jar"].concat(def.options), { cwd: this.workspaceRoot });
-			} else if (def.backend == "bc") {
+			if (def.backend.toLowerCase() == "jar") {
+				exec = new vscode.ShellExecution("java", ["-jar", baseName + ".jar"].concat(options), { cwd: this.workspaceRoot });
+			} else if (def.backend.toLowerCase() == "bc") {
 				exec = new vscode.ShellExecution(
 					"flowcpp", 
-					[baseName + ".bytecode"].concat(def.options == [] ? [] : ["--"].concat(def.options)), 
+					[baseName + ".bytecode"].concat(options == [] ? [] : ["--"].concat(options)), 
+					{ cwd: this.workspaceRoot }
+				);
+			} else if (def.backend.toLowerCase() == "js" && def.browser && def.urlPrefix) {
+				// start chrome %2 https:\\localhost\rhapsode\%1.html
+				let name = path.basename(path.join(this.workspaceRoot, def.program), ".flow");
+				let urlPrefix = def.urlPrefix + (def.urlPrefix.endsWith("/") ? "" : "/");
+				exec = new vscode.ShellExecution(
+					def.browser, 
+					options.concat([urlPrefix + name + ".html"]), 
 					{ cwd: this.workspaceRoot }
 				);
 			}
 			if (!exec) return undefined; else
-			return new vscode.Task(def, vscode.TaskScope.Workspace, `run ${def.program} ${def.backend} ${def.options.join(' ')}`,
-					RunTaskProvider.scriptType, exec);
+			return new vscode.Task(def, vscode.TaskScope.Workspace, `run ${def.program} ${def.backend} ${options.join(' ')}`,
+					RunTaskProvider.scriptType, exec, []);
 		} else {
 			return undefined;
 		}
