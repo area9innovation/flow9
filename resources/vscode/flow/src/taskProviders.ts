4fbc7477b482cdaaae78b8cd1eaca99ec2a69998
Dmitry Vlasov <vlasov@academ.org>
2020-04-29 14:33:14 +0700

    vscode: taskProvider is refactored

--- resources/vscode/flow/src/taskProviders.ts
+++ resources/vscode/flow/src/taskProviders.ts
@@ -19,34 +19,22 @@ export class BuildTaskProvider implements vscode.TaskProvider {
 		return undefined;
 	}
 
-	public resolveTask(_task: vscode.Task): vscode.Task | undefined {
-		const program: string = _task.definition.program;
-		const backend: string = _task.definition.backend;
-		if (backend && program) {
-			const definition: FlowdTaskDefinition = <any>_task.definition;
-			return this.getTask(program, backend, definition.options ? definition.options : [], definition);
+	public resolveTask(task: vscode.Task): vscode.Task | undefined {
+		const def: FlowdTaskDefinition = <any>task.definition;
+		if (def && def.program && def.backend) {
+			const def: FlowdTaskDefinition = <any>task.definition;
+			let exec = null;
+			if (def.backend == "jar") {
+				exec = new vscode.ShellExecution("flowc1", ["jar=1", def.program].concat(def.options), { cwd: this.workspaceRoot });
+			} else if (def.backend == "bc") {
+				exec = new vscode.ShellExecution("flowc1", ["bytecode=1", def.program].concat(def.options), { cwd: this.workspaceRoot });
+			}
+			if (!exec) return undefined; else
+			return new vscode.Task(def, vscode.TaskScope.Workspace, `build ${def.program} ${def.backend} ${def.options.join(' ')}`,
+					BuildTaskProvider.scriptType, exec);
+		} else {
+			return undefined;
 		}
-		return undefined;
-	}
-
-	private getTask(program: string, backend: string, options: string[], definition?: FlowdTaskDefinition): vscode.Task {
-		if (definition === undefined) {
-			definition = {
-				type: BuildTaskProvider.scriptType,
-				program,
-				backend,
-				options
-			};
-		}
-		let exec = null;
-		if (backend == "jar") {
-			exec = new vscode.ShellExecution("flowc1", ["jar=1", program].concat(options), { cwd: this.workspaceRoot });
-		} else if (backend == "bc") {
-			exec = new vscode.ShellExecution("flowc1", ["bytecode=1", program].concat(options), { cwd: this.workspaceRoot });
-		}
-		if (!exec) return undefined; else
-		return new vscode.Task(definition, vscode.TaskScope.Workspace, `${program} ${backend} ${options.join(' ')}`,
-				BuildTaskProvider.scriptType, exec);
 	}
 }
 
@@ -62,38 +50,25 @@ export class RunTaskProvider implements vscode.TaskProvider {
 		return undefined;
 	}
 
-	public resolveTask(_task: vscode.Task): vscode.Task | undefined {
-		const program: string = _task.definition.program;
-		const backend: string = _task.definition.backend;
-		if (backend && program) {
-			const definition: FlowdTaskDefinition = <any>_task.definition;
-			return this.getTask(program, backend, definition.options ? definition.options : [], definition);
+	public resolveTask(task: vscode.Task): vscode.Task | undefined {
+		const def: FlowdTaskDefinition = <any>task.definition;
+		if (def && def.program && def.backend) {
+			let baseName = path.basename(path.join(this.workspaceRoot, def.program), ".flow");
+			let exec = null;
+			if (def.backend == "jar") {
+				exec = new vscode.ShellExecution("java", ["-jar", baseName + ".jar"].concat(def.options), { cwd: this.workspaceRoot });
+			} else if (def.backend == "bc") {
+				exec = new vscode.ShellExecution(
+					"flowcpp", 
+					[baseName + ".bytecode"].concat(def.options == [] ? [] : ["--"].concat(def.options)), 
+					{ cwd: this.workspaceRoot }
+				);
+			}
+			if (!exec) return undefined; else
+			return new vscode.Task(def, vscode.TaskScope.Workspace, `run ${def.program} ${def.backend} ${def.options.join(' ')}`,
+					RunTaskProvider.scriptType, exec);
+		} else {
+			return undefined;
 		}
-		return undefined;
-	}
-
-	private getTask(program: string, backend: string, options: string[], definition?: FlowdTaskDefinition): vscode.Task {
-		if (definition === undefined) {
-			definition = {
-				type: BuildTaskProvider.scriptType,
-				program,
-				backend,
-				options
-			};
-		}
-		let baseName = path.basename(path.join(this.workspaceRoot, program), ".flow");
-		let exec = null;
-		if (backend == "jar") {
-			exec = new vscode.ShellExecution("java", ["-jar", baseName + ".jar"].concat(options), { cwd: this.workspaceRoot });
-		} else if (backend == "bc") {
-			exec = new vscode.ShellExecution(
-				"flowcpp", 
-				[baseName + ".bytecode"].concat(options == [] ? [] : ["--"].concat(options)), 
-				{ cwd: this.workspaceRoot }
-			);
-		}
-		if (!exec) return undefined; else
-		return new vscode.Task(definition, vscode.TaskScope.Workspace, `${program} ${backend} ${options.join(' ')}`,
-				BuildTaskProvider.scriptType, exec);
 	}
 }
