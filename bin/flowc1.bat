0f0e51d2857f813343cd11f9c096af6de849d3a3
Soren Frank Andersen <soren@area9.dk>
2020-09-22 12:47:16 +0200

    If java is not in the path, then give a warning

--- bin/flowc1.bat
+++ bin/flowc1.bat
@@ -1,6 +1,13 @@
 @echo off
 
 PATH %PATH%;%JAVA_HOME%\bin\
+
+where java
+if ERRORLEVEL 1 (
+	echo Java not found, please install OpenJDK 14 or newer, and set JAVA_HOME to the install location or add java to the path.
+	goto :eof
+)
+
 for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j"
 set argsFor8=
 if %jver% == 8 (set argsFor8=-XX:+UseConcMarkSweepGC -XX:ParallelCMSThreads=2)
