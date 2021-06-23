764b39ee48bff47a3ac67390e80099c0b6739273
NikitaFil <lifatikin@gmail.com>
2021-06-23 19:17:49 +0300

    Revert "Revert "JS. iPad. Scrolling by trackpad. Released from the flag.""
    This reverts commit f9e2c92e0e35a94f15fbb0e766315e367518d907.

--- platforms/js/DisplayObjectHelper.hx
+++ platforms/js/DisplayObjectHelper.hx
@@ -242,7 +242,7 @@ class DisplayObjectHelper {
 		clip.interactive = untyped clip.scrollRectListener != null || clip.isInteractive
 			|| (
 				// It allows to catch wheel events from trackpad on iPad in Safari
-				!(Platform.isIOS && Platform.isSafari && (Util.getParameter("new") == "1" || Util.getParameter("trackpad_scroll") == "1"))
+				!(Platform.isIOS && Platform.isSafari && Util.getParameter("trackpad_scroll") != "0")
 				&& (clip.listeners("pointerout").length > 0 || clip.listeners("pointerover").length > 0 || clip.cursor != null)
 			);
 		clip.interactiveChildren = clip.interactive || interactiveChildren;
