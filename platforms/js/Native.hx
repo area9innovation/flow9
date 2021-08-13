66b4fd6b94ff338e0c0420f6520daf8d114b19c1
Yuriy Kuzmenko <yuriy.kuzmenko@area9.dk>
2021-08-13 17:58:14 +0300

    fix cog9 build

--- platforms/js/Native.hx
+++ platforms/js/Native.hx
@@ -214,7 +214,14 @@ class Native {
 			untyped textArea.select();
 
 			try {
-				RenderSupport.disablePasteEventListener();
+				#if js
+				untyped __js__("
+					if (typeof RenderSupport !== 'undefined') {
+						RenderSupport.disablePasteEventListener();
+					}
+				");
+				#end
+
 				var successful = Browser.document.execCommand('paste');
 
 				if (successful) {
@@ -225,7 +232,14 @@ class Native {
 			} catch (err : Dynamic) {
 				Errors.report('Oops, unable to paste');
 			}
-			RenderSupport.enablePasteEventListener();
+
+			#if js
+			untyped __js__("
+				if (typeof RenderSupport !== 'undefined') {
+					RenderSupport.enablePasteEventListener();
+				}
+			");
+			#end
 
 			Browser.document.body.removeChild(textArea);
 
