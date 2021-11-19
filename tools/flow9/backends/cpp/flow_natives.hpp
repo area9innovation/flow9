4747f1c75a28faeef4da9a6a61732452aef94319
Yulia Shkel <yulia.shkel@area9.dk>
2021-11-19 16:11:11 +0300

    Flow9 : fixes for c++ (#914)

--- tools/flow9/backends/cpp/flow_natives.hpp
+++ tools/flow9/backends/cpp/flow_natives.hpp
@@ -2,6 +2,8 @@
 #include <codecvt>
 #include <string>
 #include <locale>
+#include <sstream>
+#include <iomanip>
 // math
 #include <cmath>
 
@@ -56,6 +58,10 @@ int32_t flow_bitUshr(int32_t a, int32_t n) {
 	return ((unsigned int)a) >> n;
 }
 
+int32_t flow_trunc(double v) {
+	return (int32_t)v;
+}
+
 // string
 
 std::u16string flow_substring(std::u16string s, int32_t start, int32_t length) {
@@ -68,6 +74,16 @@ int32_t flow_strlen(std::u16string s) {
 
 int32_t flow_getCharCodeAt(std::u16string s, int32_t i) {
 	return s.at(i);
+}
+
+// precision = 20!
+std::u16string flow_d2s(double v) {
+	std::stringstream stream;
+	stream << std::fixed << std::setprecision(20) << v;
+	std::string s = stream.str();
+
+	std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> codecvt;
+	return codecvt.from_bytes(s);
 }
 
 // array
@@ -93,21 +109,26 @@ std::vector<int32_t> flow_enumFromTo(int32_t start, int32_t end) {
 	}
 }
 
+
 template <typename A, typename B>
 std::vector<B> flow_map(const std::vector<A> flow_a, const std::function<B(A)> & flow_fn) {
-  std::vector<int> res(flow_a.size());
-  for (std::size_t i = 0; i != flow_a.size(); ++i) {
-    res[i] = flow_fn(flow_a[i]);
-  }
-  return res;
+  // std::vector<B> res(flow_a.size());
+  // for (std::size_t i = 0; i != flow_a.size(); ++i) {
+  //   res[i] = flow_fn(flow_a[i]);
+  // }
+  //return res;
+	std::vector<B> res;
+	std::transform(flow_a.begin(), flow_a.end(), std::back_inserter(res), flow_fn);
+	return res;
 }
 
 template <typename A>
 std::vector<A> flow_filter(const std::vector<A> flow_a, const std::function<bool(A)> & flow_test) {
-  std::vector<int> res;
+  std::vector<A> res;
   std::copy_if (flow_a.begin(), flow_a.end(), std::back_inserter(res), flow_test);
   return res;
 }
+
 
 template <typename A>
 std::vector<A> flow_concat(const std::vector<A> flow_a, const std::vector<A> flow_b) {
@@ -120,6 +141,11 @@ std::vector<A> flow_concat(const std::vector<A> flow_a, const std::vector<A> flo
 
 
 // flowstruct
+template <typename A, typename B>
+bool flow_isSameStructType(A struct1, B struct2) {
+	return struct1._id == struct2._id;
+}
+
 template <typename A, typename ...Args2>
 bool flow_isSameStructType(A struct1, std::variant<Args2...> struct2) {
 	unsigned int id2 = std::visit([&](auto&& x) {return x._id;}, struct2);
@@ -137,11 +163,6 @@ bool flow_isSameStructType(std::variant<Args1...> struct1, std::variant<Args2...
 	unsigned int id1 = std::visit([&](auto&& x) {return x._id;}, struct1);
 	unsigned int id2 = std::visit([&](auto&& x) {return x._id;}, struct2);
 	return id1 == id2;
-}
-
-template <typename A, typename B>
-bool flow_isSameStructType(A struct1, B struct2) {
-	return struct1._id == struct2._id;
 }
 
 template <typename A, typename ...B> A _extractStructVal(std::variant<B...> v) { return std::get<A>(v); }
