732e45996475dcf399ebe1a1201b4763bf5c5f65
Dmitry Vlasov <vlasov@academ.org>
2021-12-23 18:20:03 +0700

    flowc: js: SyncedStore is updated

--- tools/flowc/tests/js/synced_store/SyncedStoreNativeHost.ts
+++ tools/flowc/tests/js/synced_store/SyncedStoreNativeHost.ts
@@ -1,11 +1,14 @@
-import * as typenames from "SyncedStoreNativeHost-types.d";
+import * as typenames from "./SyncedStoreNativeHost-types.d";
 
 import { syncedStore, getYjsValue } from "@syncedstore/core";
 import { WebrtcProvider } from "y-webrtc";
 
 // Create your SyncedStore store
 // We create a store which contains an array (myArray) and an object (myObject)
-export const store = syncedStore({ myArray: []});
+
+type Todo = { completed: boolean, title: string };
+
+export const store = syncedStore({ myArray: [] as Todo[], fragment: "xml"});
 
 // Create a document that syncs automatically using Y-WebRTC
 const doc = getYjsValue(store);
