fc2a3772239b23db11cfcf2910c7179faaf8c482
shkel <yulia.shkel@area9.dk>
2022-01-20 11:09:39 +0300

    add the base type (_FlowUnion) for Unions

---
+++ tools/flow9/backends/cpp/flow_union.hpp
@@ -0,0 +1,27 @@
+#include <variant>
+#include "flow_memory.hpp"
+
+
+template <typename ...T>
+struct _FlowUnion : std::variant<T...> {
+	int _counter = 1;
+	~_FlowUnion() {
+		std::cout << (_counter == 0 ? "" : " !!ERROR!! ") << " ~ destroy Uni_FlowUnionon1; counter=" << _counter << " &=" << this << " ~ " << std::endl;
+		dropFields();
+	}
+	//bool operator==(const _FlowUnion& a) const { return areValuesEqual(*this, a); }
+
+	template <typename F>
+	decltype(auto) visit(F&& f)& {
+		return std::visit(std::forward<F>(f), static_cast<_FlowUnion::variant&>(*this));
+	}
+	void dupFields() {
+		// std::visit([](auto&& a) { dup(a); }, *this); // VS (win) compiler
+		(*this).visit([&](auto&& v) { dup(v); }); // g++ -std=c++2a ...
+	}
+	void dropFields() {
+		// std::visit([](auto&& a) { drop(a); }, *this);
+		(*this).visit([&](auto&& v) { drop(v); });
+	}
+};
+
