94f906ac74e564e8f1309a38e2a8f8ff9916a9b1
Kirill Bykov <brawaga@gmail.com>
2022-02-14 16:12:10 +0700

    PositionSelection added and refactored, https://trello.com/c/Gs9MMMXx/14656-wigi-implement-asian-languages-autocomplete-in-wigi-editor

--- platforms/js/TextClip.hx
+++ platforms/js/TextClip.hx
@@ -728,7 +728,11 @@ class TextClip extends NativeWidgetClip {
 
 		// Force text value right away
 		if (nativeWidget != null && isInput) {
+			var keepSelectionRange = true;
+			var selectionStartPrev = nativeWidget.selectionStart;
+			var selectionEndPrev = nativeWidget.selectionEnd;
 			nativeWidget.value = text;
+			if (keepSelectionRange) setSelection(selectionStartPrev, selectionEndPrev);
 		}
 
 		if (this.isHTMLRenderer()) {
@@ -1333,7 +1337,7 @@ class TextClip extends NativeWidgetClip {
 			if ((Platform.isChrome || Platform.isEdge) && decimalSeparatorFix) {
 				nativeWidget.value = '';
 			}
-			nativeWidget.value = newValue;
+			nativeWidget.value = val;
 		}
 
 		if (newValue != nativeWidgetValue) {
