c0dd9b6741241f362f135fb1294ec6cca847c712
Pavel Zherdin <pavel@area9.dk>
2022-06-06 16:49:59 +0200

    Add placeholders for HistoryApi for java
    Want to compile jars that rely on them

---
+++ platforms/java/com/area9innovation/flow/HistoryApi.java
@@ -0,0 +1,14 @@
+package com.area9innovation.flow;
+
+
+public class HistoryApi extends NativeHost {
+	public static String getBrowserURL() {
+		System.out.println("getBrowserURL not implemented");
+		return "";
+	}
+
+
+	public static void replaceState(String data, String title, String[] url) {
+		System.out.println("replaceState not implemented");
+	}
+}
