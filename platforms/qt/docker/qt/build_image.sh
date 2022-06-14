558ed2d8ce7fd8f176779e0acdce2fe80a065e67
Pavel Zherdin <pavel@area9.dk>
2022-06-14 17:09:58 +0200

    Add ubuntu version to area9/qt tag

--- platforms/qt/docker/qt/build_image.sh
+++ platforms/qt/docker/qt/build_image.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-qt_version=5.15.2
+qt_version="5.15.2"
+ubuntu_version="focal"
 
 registry=""
 if [ -n "$1" ]; then
   registry="$1/"
 fi
 
-docker build --build-arg QT="${qt_version}" -t "${registry}area9/qt:aqt-${qt_version}" .
+docker build --build-arg QT="${qt_version}" -t "${registry}area9/qt:aqt-${ubuntu_version}-${qt_version}" .
 
