d1200a21318fff4785577bd8ef3e349dce7c51d3
Dmitry Vlasov <vlasov@academ.org>
2022-07-18 19:00:49 +0200

    html: a gringo-based HTML parser is added

--- lib/formats/html/parser/compile_grammar.sh
+++ lib/formats/html/parser/compile_grammar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Run from the current directory
 cd ../../..
 
 flowcpp --batch lingo/pegcode/pegcompiler.flow -- flowparser=formats/html/parser/parser.flow flowparserast=formats/html/parser/ast.flow file=formats/html/parser/html.lingo prefix_rules=html
