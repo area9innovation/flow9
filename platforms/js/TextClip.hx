f6efb395a06f0d9687919f5439cd48fbcdd8610a
NikitaFil <lifatikin@gmail.com>
2022-08-03 13:02:13 +0300

    iOS. TextClip. Switch on ensureInputVisible by default.
    https://trello.com/c/fAAxSZLL/18336-activity-ipad-keyboard-appears-which-obscures-response-box

--- platforms/js/TextClip.hx
+++ platforms/js/TextClip.hx
@@ -119,7 +119,7 @@ class UnicodeTranslation {
 
 class TextClip extends NativeWidgetClip {
 	public static var KeepTextClips = Util.getParameter("wcag") == "1";
-	public static var EnsureInputIOS = Util.getParameter("ensure_input_ios") == "1";
+	public static var EnsureInputIOS = Util.getParameter("ensure_input_ios") != "0";
 	public static var useLetterSpacingFix = Util.getParameter("letter_spacing_fix") != "0";
 	public static var useForcedUpdateTextWidth = Util.getParameter("new") == "1" && Util.getParameter("forced_textwidth_update") != "0";
 
