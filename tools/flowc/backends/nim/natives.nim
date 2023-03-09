e157a988cbac3b00b871b63ad2750661df0832e8
Yulia Shkel <yulia.shkel@area9.dk>
2023-03-09 10:33:12 +0100

    nim : fix mapi (newSeq)

--- tools/flowc/backends/nim/natives.nim
+++ tools/flowc/backends/nim/natives.nim
@@ -38,7 +38,7 @@ proc replace*[T](s: seq[T], i: int32, v: T): seq[T] =
 
 # Apply a function which takes an index and each element of an array to give a new array
 proc mapi*[T, S](s: seq[T], op: proc (i: int32, v: T): S): seq[S] =
-  var rv: seq[S] = newSeq(length(s))
+  var rv: seq[S] = newSeq[S](length(s))
   for i in 0 .. s.len-1:
     rv[i] = op(i, s[i])
   return rv
