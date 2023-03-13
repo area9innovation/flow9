6a4432e0f4cd7fc80f2faaa46220b78fa24a82d2
shkel <yulia.shkel@area9.dk>
2023-03-13 15:18:11 +0100

    nim : native stubs

---
+++ tools/flowc/backends/nim/natives/dayOfWeek.nim
@@ -0,0 +1,4 @@
+# dayOfWeek1 : io (year: int, month: int, day: int) -> int = Native.dayOfWeek;
+
+func dayOfWeek*(year: int32, month: int32, day: int32): int32 =
+  return -1
---
+++ tools/flowc/backends/nim/natives/getCurrentDate.nim
@@ -0,0 +1,5 @@
+# getCurrentDate : io () -> Date = Native.getCurrentDate;
+# Date(year : int, month : int, day : int);
+
+func getCurrentDate*(): float =
+  return 0.0
