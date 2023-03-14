27f85e9c70bbe8db377a5f6bec4a2637d326803b
shkel <yulia.shkel@area9.dk>
2023-03-14 13:12:11 +0100

    nim backend : first version of timer

--- tools/flowc/backends/nim/natives/timer.nim
+++ tools/flowc/backends/nim/natives/timer.nim
@@ -1,4 +1,14 @@
 # timer : io (int, () -> void) -> void = Native.timer;
 
-proc timer*(delay : int32, fn : proc (): void): void =
-#   
+import asyncdispatch
+
+proc timerFuture(delay : int32, fn : proc (): void {.gcsafe.}) {.async.} =
+  var systemTimer = sleepAsync(int(delay))
+  addCallback(systemTimer, fn)
+  try:
+    await systemTimer
+  except CatchableError:
+    discard # ignore error
+
+proc timer*(delay : int32, fn : proc (): void {.gcsafe.}) : void =
+  asyncCheck timerFuture(delay, fn)
