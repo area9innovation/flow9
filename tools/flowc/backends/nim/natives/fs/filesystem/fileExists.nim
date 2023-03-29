134792b59c12ab265f4018a7312d097da16af5cb
Dmitry Vlasov <vlasov@academ.org>
2023-03-29 18:01:06 +0200

    flowc: nim: some natives are fixed

--- tools/flowc/backends/nim/natives/fs/filesystem/fileExists.nim
+++ tools/flowc/backends/nim/natives/fs/filesystem/fileExists.nim
@@ -1,2 +1,4 @@
-proc fileExists*(path : string): bool =
-  return fileExists(path)
+# already exists in nim environment
+
+#proc fileExists*(path : string): bool =
+#  return fileExists(path)
--- tools/flowc/backends/nim/natives/net/url_parameter/getAllUrlParametersArray.nim
+++ tools/flowc/backends/nim/natives/net/url_parameter/getAllUrlParametersArray.nim
@@ -1,2 +1,11 @@
+import os
+
 proc getAllUrlParametersArray*(): seq[seq[string]] =
-  return newSeq[seq[string]]()
+  var params = filter(commandLineParams(), proc(param: string): bool = param.len > 0)
+  return map(params, proc(param: string): seq[string] =
+    let p = split(param, "=")
+    if p.len == 1:
+      return @[param, ""]
+    else:
+      return @[p[0], p[1]]
+  )
