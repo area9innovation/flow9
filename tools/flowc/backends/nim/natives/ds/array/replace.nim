6a6e8773ef477363565548019cfd294abd632ddc
Dmitry Vlasov <vlasov@academ.org>
2023-03-30 18:01:35 +0200

    flowc: nim: a bug in replace native is fixed

--- tools/flowc/backends/nim/natives/ds/array/replace.nim
+++ tools/flowc/backends/nim/natives/ds/array/replace.nim
@@ -1,7 +1,7 @@
 # Replace a given element in an array with a new value. Makes a copy
 #native replace : ([?], int32, ?) -> [?] = Native.replace;
 proc replace*[T](s: openArray[T], i: int32, v: T): seq[T] =
-  if i < 0 or s.len == 0:
+  if i < 0:
     return @[]
   else:
     var s1 = @s # Copy of s
