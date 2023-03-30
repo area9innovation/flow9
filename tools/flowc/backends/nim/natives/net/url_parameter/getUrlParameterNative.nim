e68d43a72aa91d72eff281aeebe533922f558583
shkel <yulia.shkel@area9.dk>
2023-03-30 11:28:46 +0200

    nim backend : add getUrlParameterNative

--- tools/flowc/backends/nim/natives/net/url_parameter/getUrlParameterNative.nim
+++ tools/flowc/backends/nim/natives/net/url_parameter/getUrlParameterNative.nim
@@ -1,3 +1,12 @@
+import os
+import strutils
+
 proc getUrlParameterNative*(name: string): string =
-  echo "TODO: Implement getUrlParameter"
-  ""
+  # params from main() ?
+  when declared(commandLineParams):
+    let prefix = name & "="
+    for arg in commandLineParams():
+      if arg.startsWith(prefix):
+        return arg[prefix.len .. ^1]
+  else:
+    result = ""
