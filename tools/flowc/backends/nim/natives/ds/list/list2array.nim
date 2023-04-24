4792107145d9f5ad8fae8b7f02b1cd2c406cff37
Dmitry Vlasov <vlasov@academ.org>
2023-04-24 15:31:17 +0200

    flowc: nim: some advancces in flowc compilation

--- tools/flowc/backends/nim/natives/ds/list/list2array.nim
+++ tools/flowc/backends/nim/natives/ds/list/list2array.nim
@@ -1,4 +1,4 @@
-proc $F_0(list2array)*[T](list: List[T]): seq[T] =
+proc $F_0(list2array)*[T](list: $F_1(List)[T]): seq[T] =
   var p = list
   var r = newSeq[T]()
 
@@ -6,7 +6,7 @@ proc $F_0(list2array)*[T](list: List[T]): seq[T] =
     if p.str_id == int32(st_EmptyList):
       break
     else:
-      let cons = cast[Cons[T]](p)
+      let cons = cast[$F_1(Cons)[T]](p)
       r = @[cons.head] & r
       p = cons.tail
   return r
--- tools/flowc/backends/nim/natives/ds/list/list2string.nim
+++ tools/flowc/backends/nim/natives/ds/list/list2string.nim
@@ -1,4 +1,4 @@
-proc $F_0(list2string)*(list: List[string]): string =
+proc $F_0(list2string)*(list: $F_1(List)[string]): string =
   var p = list
   var r = ""
 
@@ -6,7 +6,7 @@ proc $F_0(list2string)*(list: List[string]): string =
     if p.str_id == int32(st_EmptyList):
       break
     else:
-      let cons = cast[Cons[string]](p)
+      let cons = cast[$F_1(Cons)[string]](p)
       r = cons.head & r
       p = cons.tail
   return r
