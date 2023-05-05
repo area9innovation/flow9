e32a596f4868fba9766af23376f650d6f62f6ab8
Dmitry Vlasov <vlasov@academ.org>
2023-05-05 13:01:58 +0200

    flowc: nim: some fixes of natives

--- tools/flowc/backends/nim/natives/net/url_parameter/getUrlParameterNative.nim
+++ tools/flowc/backends/nim/natives/net/url_parameter/getUrlParameterNative.nim
@@ -1,12 +1,3 @@
-import os
-import strutils
-
 proc $F_0(getUrlParameterNative)*(name: string): string =
-  # params from main() ?
-  when declared(commandLineParams):
-    let prefix = name & "="
-    for arg in commandLineParams():
-      if arg.startsWith(prefix):
-        return arg[prefix.len .. ^1]
-  else:
-    result = ""
+  # getUrlParameterNative  is implemented in flow_lib/url_parameters
+  return getUrlParameterNative(name)
--- tools/flowc/backends/nim/natives/string/string2utf8.nim
+++ tools/flowc/backends/nim/natives/string/string2utf8.nim
@@ -1,3 +1,3 @@
-proc $F_0(string2utf8)*(s : string): seq[int] =
+proc $F_0(string2utf8)*(s : string): seq[int32] =
   echo "Implement string2utf8"
   return @[]
