c70fe2bb81151c5964fceb2028c982e014011bda
Dmitry Vlasov <vlasov@academ.org>
2023-06-01 14:28:55 +0200

    flowc: nim: fromCharCode - upper unicode symbols are supported

--- tools/flowc/backends/nim/natives/string/fromCharCode.nim
+++ tools/flowc/backends/nim/natives/string/fromCharCode.nim
@@ -2,6 +2,10 @@ import unicode
 
 func $F_0(fromCharCode)*(code: int32): RtString =
   when use16BitString:
-    return @[Utf16Char(cast[int16](code))]
+    if (code < 0x0D800 or (0x0DFFF < code and code < 0xFFFF)):
+      return @[Utf16Char(cast[int16](code))]
+    else:
+      # must be represented as a surrogate pair
+      return rt_utf8_to_string(unicode.toUTF8(cast[Rune](code)))
   else:
     return unicode.toUTF8(cast[Rune](code))
