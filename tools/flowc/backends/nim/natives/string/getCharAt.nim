647bb224e7d3445206c6ed4908909a52e63b5e1c
Dmitry Vlasov <vlasov@academ.org>
2023-06-05 17:10:17 +0200

    flowc: getCharAt returns a single byte in utf-8 mode

--- tools/flowc/backends/nim/natives/string/getCharAt.nim
+++ tools/flowc/backends/nim/natives/string/getCharAt.nim
@@ -2,6 +2,6 @@ import unicode
 
 func $F_0(getCharAt)*(s: RtString, i: int32): RtString =
   if i >= 0 and i < rt_string_len(s):
-    return when use16BitString: @[s[i]] else: unicode.toUTF8(unicode.runeAt(s, i)) 
+    return when use16BitString: @[s[i]] else: $s[i]
   else:
     return rt_empty_string()
