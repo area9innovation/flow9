90556b2e9b0fba30aa7f6a84bc23b689aaf6e355
Dmitry Vlasov <vlasov@academ.org>
2023-06-13 19:39:12 +0200

    flowc: nim: gringo successfully compiles

--- tools/flowc/backends/nim/natives/fs/filesystem/fileExists.nim
+++ tools/flowc/backends/nim/natives/fs/filesystem/fileExists.nim
@@ -1,5 +1,4 @@
-# native fileExists : (string) -> bool = FlowFileSystem.fileExists
-# the same name function
+import os
 
 proc $F_0(fileExists)*(f: RtString): bool = 
   return fileExists(rt_string_to_utf8(f))
