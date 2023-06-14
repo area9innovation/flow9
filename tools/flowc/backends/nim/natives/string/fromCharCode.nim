892877d46775be527d017a68badb7eb22f7cea18
Dmitry Vlasov <vlasov@academ.org>
2023-06-14 14:01:24 +0200

    flowc: nim: test_encoding passes (almost)
    The diff with java: broken surrogate pairs in java
    are just ?, while in nim are �
    
    77c77
    < fromCharCode: '?'
    ---
    > fromCharCode: '�'
    79c79
    < toStringForJson(fromCharCode)): "?"
    ---
    > toStringForJson(fromCharCode)): "�"
    83,84c83,84
    < getCharAt(fromCharCode, 0): ?
    < toStringForJson(getCharAt(fromCharCode, 0))): "?"
    ---
    > getCharAt(fromCharCode, 0): �
    > toStringForJson(getCharAt(fromCharCode, 0))): "�"
    97,98c97,98
    < getCharAt(fromCharCode, 0): ?
    < toStringForJson(getCharAt(fromCharCode, 0))): "?"
    ---
    > getCharAt(fromCharCode, 0): �
    > toStringForJson(getCharAt(fromCharCode, 0))): "�"
    101,102c101,102
    < getCharAt(fromCharCode, 1): ?
    < toStringForJson(getCharAt(fromCharCode, 1))): "?"
    ---
    > getCharAt(fromCharCode, 1): �
    > toStringForJson(getCharAt(fromCharCode, 1))): "�"
    129,130c129,130
    < getCharAt(fromCharCode, 0): ?
    < toStringForJson(getCharAt(fromCharCode, 0))): "?"
    ---
    > getCharAt(fromCharCode, 0): �
    > toStringForJson(getCharAt(fromCharCode, 0))): "�"
    133,134c133,134
    < getCharAt(fromCharCode, 1): ?
    < toStringForJson(getCharAt(fromCharCode, 1))): "?"
    ---
    > getCharAt(fromCharCode, 1): �
    > toStringForJson(getCharAt(fromCharCode, 1))): "�"

--- tools/flowc/backends/nim/natives/string/fromCharCode.nim
+++ tools/flowc/backends/nim/natives/string/fromCharCode.nim
@@ -5,6 +5,9 @@ proc $F_0(fromCharCode)*(code: int32): RtString =
     if (code < 0x0D800 or (0x0DFFF < code and code < 0xFFFF)):
       # a single 16-bit char
       return @[Utf16Char(cast[int16](code))]
+    elif code <= 0xFFFF:
+      # a single surrogate pair component - keep it as is
+      return @[Utf16Char(cast[int16](code))]
     else:
       # must be represented as a surrogate pair
       return rt_utf8_to_string(unicode.toUTF8(cast[Rune](code)))
