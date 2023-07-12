eeb838b802d2b220263336de3443e98045c607f0
Dmitry Vlasov <vlasov@academ.org>
2023-07-12 21:41:01 +0200

    flowc: cpp3: specific tests are added

---
+++ tools/flowc/tests/cpp3_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+flowc1 run=btester back=cpp3 cpp3-back-opts=cpp-debug test=cpp3 valgrind=1 exclude=test_runner,int_array
+flowc1 run=btester back=cpp3 cpp3-back-opts=cpp-debug test=nim  valgrind=1 exclude=gc,http test-opts=determ
+#flowc1 run=btester back=cpp3                          test=cpp3 valgrind=1 exclude=test_runner,int_array test-opts=determ
