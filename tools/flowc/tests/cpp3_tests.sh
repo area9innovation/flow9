2d3ca91c82d4c8829f1bd3b47532521c6a1a7540
Dmitry Vlasov <vlasov@academ.org>
2023-09-13 16:05:10 +0200

    flowc: cpp3: test suite is updated

--- tools/flowc/tests/cpp3_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-flowc1 run=btester back=cpp3 test=cpp3 valgrind=1 exclude=test_runner                       test-opts=determ #cpp3-back-opts=cpp-debug
-flowc1 run=btester back=cpp3 test=nim  valgrind=1 exclude=gc,http,test_local2utc,users_tree test-opts=determ #cpp3-back-opts=cpp-debug
-#flowc1 run=btester back=cpp3                          test=cpp3 valgrind=1 exclude=test_runner,int_array test-opts=determ
