1e62ee820a64f5ea59ab3da3a18d50ed025fde15
Dmitry Vlasov <vlasov@academ.org>
2023-12-05 14:19:01 +0100

    Flowc cpp3 advances (#1282)
    * flowc: cpp3: default build jobs is 4
    
    * flowc: cmake builder is repaired, tailcall is enhanced
    
    * flowc: cpp3: tests are a bit refactored
    
    * flowc: flowc.jar is updated
    
    * flowc: stats show min/max values
    
    * flowc: cpp3: fiComparisonWithConst is fixed
    
    * flowc: cpp3: some tests are added
    
    * flowc: cpp3: cpp-mimalloc is on by default
    
    * flowc: cpp3: compare of scalars is extracted
    
    * flowc: cpp3: switch uses non-virtual structId function
    
    * flowc: cpp3: ds_tree.nats is added
    
    * flowc: cpp3: treeLeftRotation/treeRightRotation made native
    
    * flowc: cpp3: rebalanceTree1 native is added and works
    
    * flowc: cpp3: setTree1 native is added, works
    
    * flowc: cpp3: tree left/right rotations are optimal
    
    * flowc: cpp3: rebalanceTree1 is optimized (mem reusing)
    
    * flowc: cpp3: Struct get doesn't affect RC, getRc1 does
    
    * flowc: cpp3: setTree is made native, uses FIP
    
    * flowc: cpp3: setTree natives are optimized
    
    * flowc: cpp3: some more optimizations of setTree natives
    
    * flowc: cpp3: bug in lookupTree native is fixed
    
    * flowc: cpp3: setTree is a bit enhanced
    
    * flowc: cpp3: redundant natices are removed
    
    * flowc: cpp3: benchmark_tree is updated
    
    * flowc: flowc.jar is updated

--- tools/flowc/backends/cpp3/runtimes/__flow_runtime_union.hpp
+++ tools/flowc/backends/cpp3/runtimes/__flow_runtime_union.hpp
@@ -5,6 +5,12 @@
 namespace flow {
 
 struct Union : public Flow {
+	Union(TypeId struct_id): struct_id_(struct_id) { }
+	TypeId structId() const { return struct_id_; }
+	virtual Int compare(Union* v) = 0;
+	virtual Union* clone() = 0;
+private:
+	const TypeId struct_id_;
 };
 
 }
