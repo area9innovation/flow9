ebf3b6029a632bf3980e1f4bd2134950177de97f
Asger Palm <asger@area9.dk>
2023-12-13 13:44:57 +0100

    Add prefix to HTML parser to avoid name clashes

--- lib/formats/html/parser/compile_grammar.sh
+++ lib/formats/html/parser/compile_grammar.sh
@@ -3,4 +3,4 @@
 # Run from the current directory
 cd ../../..
 
-gringo file=formats/html/parser/html.gringo compile=1 types=1 type-prefix=PlainHtml master-type=PlainHtmlAst
+gringo file=formats/html/parser/html.gringo compile=1 types=1 type-prefix=PlainHtml master-type=PlainHtmlAst prefix=html_
