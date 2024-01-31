3398a9d15eafb969aba5ab20832ee1c3022173da
Dmitry Vlasov <vlasov@academ.org>
2024-01-31 16:25:47 +0100

    Flowc cpp3 implicit union typars (#1302)
    * flowc: fiCompleteImplicitUnionTypars program transformation is added
    
    * flowc: cpp3: benchmark_inlining is removed from a test suite

--- tools/flowc/tests/cpp3_tests_flowc1.sh
+++ tools/flowc/tests/cpp3_tests_flowc1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## flowc1 tests:
-flowc1 run=btester back=cpp3 compiler=flowc1 test=cpp3 valgrind=1 exclude=test_runner,test_concur,sizeof,test_clone    test-opts=determ #cpp3-back-opts=cpp-debug
+flowc1 run=btester back=cpp3 compiler=flowc1 test=cpp3 valgrind=1 exclude=test_runner,test_concur,sizeof,test_clone,benchmark_inlining    test-opts=determ #cpp3-back-opts=cpp-debug
 flowc1 run=btester back=cpp3 compiler=flowc1 test=nim  valgrind=1 exclude=gc,http,test_local2utc,users_tree test-opts=determ #cpp3-back-opts=cpp-debug
 
 #flowc1 run=btester back=cpp3                          test=cpp3 valgrind=1 exclude=test_runner,int_array test-opts=determ
