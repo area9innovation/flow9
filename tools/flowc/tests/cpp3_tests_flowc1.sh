a6801adebceda264756d71b2b6f19e63f87cc3f0
Dmitry Vlasov <vlasov@academ.org>
2024-02-13 13:25:03 +0100

    flowc: compile-time computation is enhanced

--- tools/flowc/tests/cpp3_tests_flowc1.sh
+++ tools/flowc/tests/cpp3_tests_flowc1.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
 ## flowc1 tests:
-flowc1 run=btester back=cpp3 compiler=flowc1 test=cpp3 valgrind=1 exclude=test_runner,test_concur,sizeof,test_clone,benchmark_inlining    test-opts=determ #cpp3-back-opts=cpp-debug
-flowc1 run=btester back=cpp3 compiler=flowc1 test=nim  valgrind=1 exclude=gc,http,test_local2utc,users_tree test-opts=determ #cpp3-back-opts=cpp-debug
+flowc1 \
+	run=btester \
+	back=cpp3 \
+	compiler=flowc1 \
+	test=cpp3 \
+	valgrind=1 \
+	java-flowc-opts=optimize \
+	cpp3-flowc-opts=optimize \
+	exclude=test_runner,test_concur,sizeof,test_clone,benchmark_inlining \
+	test-opts=determ
+
+#cpp3-back-opts=cpp-debug
+flowc1 \
+	run=btester \
+	back=cpp3 \
+	compiler=flowc1 \
+	test=nim \
+	valgrind=1 \
+	java-flowc-opts=optimize \
+	cpp3-flowc-opts=optimize \
+	exclude=gc,http,test_local2utc,users_tree \
+	test-opts=determ #cpp3-back-opts=cpp-debug
 
 #flowc1 run=btester back=cpp3                          test=cpp3 valgrind=1 exclude=test_runner,int_array test-opts=determ
 
