da0375c045d7e6230fb6a3f68ab7415f6d1ca52d
NikitaFil <lifatikin@gmail.com>
2024-03-07 17:17:17 +0200

    Fixed build.
    https://trello.com/c/DMSFQ1ty/23930-japanese-translations-dont-have-bold-text

--- platforms/js/Platform.hx
+++ platforms/js/Platform.hx
@@ -78,6 +78,8 @@ class Platform {
 	private static function getIsHighDensityDisplay(){
 		return untyped __js__("((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3))");
 	}
+
+	private static function getIsWindows() { return isWindows; }
 #end
 }
 
