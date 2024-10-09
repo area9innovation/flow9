f0b57aacb8e57c976c6234dcf050fc863346dadc
Vadim Lisichenkov <vadim.lisichenkov@area9.dk>
2024-10-09 16:39:05 +0300

    implemented validation string operations in java target. In fact - copied from security.php

--- platforms/java/com/area9innovation/flow/FlowPassword.java
+++ platforms/java/com/area9innovation/flow/FlowPassword.java
@@ -17,18 +17,22 @@ import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 import java.util.Arrays;
 import java.util.Random;
+import org.json.JSONObject;
 
 public class FlowPassword extends NativeHost {
 	public static final String PBKDF2_HASH_ALGORITHM = "sha256";
 	public static final int PBKDF2_ITERATIONS = 310000;
 	public static final int PBKDF2_SALT_BYTE_SIZE = 24;
 	public static final int PBKDF2_HASH_BYTE_SIZE = 24;
+	public static final int PBKDF2_RANDOM_PASSWORD_BYTE_SIZE = 24;
 
 	public static final int HASH_SECTIONS = 4;
 	public static final int HASH_ALGORITHM_INDEX = 0;
 	public static final int HASH_ITERATION_INDEX = 1;
 	public static final int HASH_SALT_INDEX = 2;
 	public static final int HASH_PBKDF2_INDEX = 3;
+
+	public static final int VALIDATION_DEFAULT_EXP = 86400;
 
 	public static String createSalt() {
 		byte[] bytes = new byte[PBKDF2_SALT_BYTE_SIZE];
@@ -102,4 +106,68 @@ public class FlowPassword extends NativeHost {
 			
 		return Arrays.equals(correctpbkdf2, requestedpbkdf2);
 	}
+	
+	public static String getPasswordValidationString(int exp) {
+		Integer expLocal = exp < 0 ? null : ( exp == 0 ? VALIDATION_DEFAULT_EXP : exp);
+
+		byte[] bytes = new byte[PBKDF2_RANDOM_PASSWORD_BYTE_SIZE];
+		new SecureRandom().nextBytes(bytes);
+
+		String validation = "";
+
+		try {
+			JSONObject jo = new JSONObject();
+			jo.put("key", Base64.getEncoder().encodeToString(bytes));
+			if (expLocal != null) {
+				long unixTime = System.currentTimeMillis() / 1000L;
+				jo.put("exp", unixTime + expLocal);
+			};
+
+			validation = strtr(Base64.getEncoder().encodeToString(jo.toString().getBytes()), "+/=", "*_-");
+		} catch (Exception e) {
+			System.out.println("getPasswordValidationString error: " + e.toString());
+		}
+
+		return validation;
+	}
+
+	public static int getValidationStringExpiration(String validation) {
+		int expiration = 0;
+
+		try {
+			byte[] decoded = Base64.getDecoder().decode(strtr(validation, "*_-", "+/="));
+			// String decodedString = new String(decoded, StandardCharsets.UTF_8);
+			String decodedString = new String(decoded);
+
+			JSONObject decodedJson = new JSONObject(decodedString);
+
+			expiration = decodedJson.optInt("exp", 0);
+		} catch (Exception e) {
+			System.out.println("getValidationStringExpiration error: " + e.toString());
+		}
+
+		return expiration;
+	}
+
+	public static Boolean checkValidationStringExpiration(String validation, int leeway) {
+		int leewayLocal = leeway <= 0 ? 0 : leeway;
+
+		int expiration = getValidationStringExpiration(validation);
+		long unixTime = System.currentTimeMillis() / 1000L;
+		return expiration == 0 || expiration > (unixTime + leewayLocal);
+	}
+
+	public static String strtr(String str, String froms, String tos) { 
+		char[] str1 = str.toCharArray();
+
+		for (int i = 0, len = str.length(); i < len; ++i){
+			int j = froms.indexOf(str.charAt(i));
+			if (j != -1)
+				str1[i] = tos.charAt(j);
+		}
+
+		return String.valueOf(str1);
+	}
+/*
+	*/
 }
