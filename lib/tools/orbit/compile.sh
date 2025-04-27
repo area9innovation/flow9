555fbfff374142ae2af3d939512901ac62f3ea0f
alstrup <asger@area9.dk>
2025-04-27 06:24:12 +0200

    Start an sexpr based scheme-like language

--- lib/tools/orbit/compile.sh
+++ lib/tools/orbit/compile.sh
@@ -3,10 +3,15 @@
 # Compile the orbit grammar
 flowcpp --batch tools/mango/mango.flow -- grammar=orbit.mango types=2 typeprefix=Or prefix=orbit_ compile=1 vscode=1 vsname="Orbit" extension=orb
 
-echo "Compilation complete. Run package-vscode.sh to update VS code plugin."
+echo "Compilation complete. Run package-vscode.sh to update VS Code plugin."
 
 # Run the packaging script automatically if requested
 if [ "$1" = "--package" ]; then
   echo "Running VS Code extension packaging script..."
   ./package-vscode.sh
 fi
+
+# Compile the orbit S-expression grammar
+cd sexpr
+flowcpp --batch tools/mango/mango.flow -- grammar=sexpr.mango types=2 typeprefix=S prefix=sexpr_ compile=1
+cd ..
