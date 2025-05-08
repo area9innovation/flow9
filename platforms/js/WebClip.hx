dbf60e0381f8bdfc583fb12348d6358d9d4aeb17
NikitaFil <nikita.fil@area9.dk>
2025-05-08 11:05:31 +0300

    Revert "JS. WebClip. Added allow="autoplay" under a flag"
    This reverts commit ba11bdb6d6d07f31415ff99b40fdf9bf748d0347.

--- platforms/js/WebClip.hx
+++ platforms/js/WebClip.hx
@@ -85,10 +85,6 @@ class WebClip extends NativeWidgetClip {
 		iframe.allowFullscreen = true;
 		iframe.frameBorder = "no";
 		iframe.callflow = cb; // Store for crossdomain calls
-		
-		if (Util.getParameter("allowautoplay") == "1") {
-			iframe.allow = "autoplay";
-		}
 
 		iframe.id = nativeWidget.id + "_iframe";
 		nativeWidget.appendChild(iframe);
