80918a42afdd5cdb6674b76709de882fa8d4e5c3
Asger Palm <asger@area9.dk>
2025-05-14 08:24:16 +0200

    Correct output for these tests

---
+++ lib/tools/orbit/sexpr/GLSL/tests/basic7_eq_data.glsl
@@ -0,0 +1,28 @@
+// Auto-generated GLSL code for S-expression interpreter
+// AST size: 27 elements
+// Constant Pool size: 6 elements
+
+#include "glsl_sexpr_defines.h"
+
+// Program size information
+#define PROGRAM_AST_SIZE 27
+#define CONSTANT_POOL_SIZE 6
+
+const float u_constant_pool[6] = float[](
+    1.0f, 43.0f, 1.0f, 45.0f,
+    1.0f, 61.0f
+);
+
+const float u_program_ast[27] = float[](
+    10.0f, 2.0f, 1.0f, 4.0f,  // [0] begin, count=1, child_offset=4
+    8.0f, 3.0f, 7.0f,  // [4] list, count=3, child_offset=7
+    7.0f, 2.0f,  // [7] = (operator)
+    8.0f, 3.0f, 12.0f,  // [9] list, count=3, child_offset=12
+    7.0f, 0.0f,  // [12] + (operator)
+    1.0f, 10.0f,  // [14] 10 (int)
+    1.0f, 5.0f,  // [16] 5 (int)
+    8.0f, 3.0f, 21.0f,  // [18] list, count=3, child_offset=21
+    7.0f, 1.0f,  // [21] - (operator)
+    1.0f, 20.0f,  // [23] 20 (int)
+    1.0f, 5.0f  // [25] 5 (int)
+);
