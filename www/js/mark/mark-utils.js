6ccaa39d774e001406e167c07c903b8dd8e30cfe
Massimiliano Missiato <massimiliano.missiato@area9.dk>
2025-05-20 17:06:29 +0100

    Introduce MMark
    highlight relevant text using mark.js

---
+++ www/js/mark/mark-utils.js
@@ -0,0 +1,39 @@
+/*
+	Utilities for mark.min.js
+*/
+
+function getMarkInstance(querySelectorField) {
+	console.log('Getting Mark.js instance for selector:', querySelectorField);
+
+	const rootElement = document.querySelector('.' + querySelectorField);
+	if (!rootElement) {
+		console.warn('Root element not found for selector:', querySelectorField);
+		return null;
+	}
+
+	const textWidgets = rootElement.querySelectorAll('.textWidget');
+	return new Mark(textWidgets);
+}
+
+function performMark(keyword, querySelectorField) {
+	// Util.getParameter("devtrace") == "1"
+	console.log('Mark.js performMark');
+	const markInstance = getMarkInstance(querySelectorField);
+	if (!markInstance) return;
+
+	const options = {};
+	markInstance.unmark({
+		done: function () {
+			markInstance.mark(keyword, options);
+		}
+	});
+}
+
+function performUnMark(querySelectorField) {
+	// Util.getParameter("devtrace") == "1"
+	console.log('Mark.js performUnMark');
+	const markInstance = getMarkInstance(querySelectorField);
+	if (!markInstance) return;
+
+	markInstance.unmark();
+}
