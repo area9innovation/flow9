area9innovation/flow9

commit 18a0df69dd502b397984174e2101e3767b834fa9

JS. TextClip. Support font family with spaces.
If font family name has spaces, it should be wrapped in quote to be handled by browser.
https://trello.com/c/sxETUSi3/28436-source-sans-3-google-font-spacing-and-padding-issues

platforms/js/TextClip.hx
@@ -711,6 +711,9 @@ class TextClip extends NativeWidgetClip {
 			return fontSlope == "italic" ? "Italic" : fontWeight == 700 ? "Bold" : "Book";
 		} else if (StringTools.startsWith(fontFamily, "'Roboto")) {
 			return fontFamily + fontWeightToString(fontWeight) + fontSlope == "normal" ? "" : capitalize(fontSlope);
+		} else if (StringTools.contains(fontFamily, " ") && !StringTools.startsWith(fontFamily, "'")) {
+			// If fontFamily contains space, it is not a valid CSS font-family name, so we need to quote it.
+			return "'" + fontFamily + "'";
 		} else {
 			return fontFamily;
 		}

commit 89285a59550388c0217c6f5b175963e686ecc550

JS. TextClip. Fixed wrong metrics and positioning for Material symbols.
Regression after https://github.com/area9innovation/flow9/commit/18a0df69dd502b397984174e2101e3767b834fa9

Icon in MIconButton was positioned at the top right corner of the button box.
https://trello.com/c/HzV0xAYO/28445-manage-organization-button-in-organizations-list-is-positioned-strangely

platforms/js/TextClip.hx
@@ -711,7 +711,13 @@ class TextClip extends NativeWidgetClip {
 			return fontSlope == "italic" ? "Italic" : fontWeight == 700 ? "Bold" : "Book";
 		} else if (StringTools.startsWith(fontFamily, "'Roboto")) {
 			return fontFamily + fontWeightToString(fontWeight) + fontSlope == "normal" ? "" : capitalize(fontSlope);
-		} else if (StringTools.contains(fontFamily, " ") && !StringTools.startsWith(fontFamily, "'")) {
+		} else {
+			return quoteFontFamilyIfSpaces(fontFamily);
+		}
+	}
+
+	private static inline function quoteFontFamilyIfSpaces(fontFamily : String) : String {
+		if (StringTools.contains(fontFamily, " ") && !StringTools.startsWith(fontFamily, "'")) {
 			// If fontFamily contains space, it is not a valid CSS font-family name, so we need to quote it.
 			return "'" + fontFamily + "'";
 		} else {
@@ -1879,7 +1885,7 @@ class TextClip extends NativeWidgetClip {
 			if (!TextClip.onFontLoadedListenerInitialized) {
 				Browser.document.fonts.addEventListener('loadingdone', function(event : Dynamic) {
 					event.fontfaces.forEach(function(fontface, key, set) {
-						var fontFamilySet = TextClip.scheduledForceUpdateTree.get(fontface.family);
+						var fontFamilySet = TextClip.scheduledForceUpdateTree.get(quoteFontFamilyIfSpaces(fontface.family));
 						if (fontFamilySet != null) {
 							fontFamilySet.forEach(function(clip : TextClip, key, set) {
 								clip.forceUpdateTextWidth();
@@ -2210,7 +2216,8 @@ class TextClip extends NativeWidgetClip {
 	}
 
 	private function isMaterialIconFont() : Bool {
-		return style.fontFamily.startsWith('Material Icons') || style.fontFamily.startsWith('Material Symbols');
+		return style.fontFamily.startsWith('Material Icons') || style.fontFamily.startsWith('Material Symbols')
+			|| style.fontFamily.startsWith("'Material Icons") || style.fontFamily.startsWith("'Material Symbols");
 	}
 
 	private function getSanitizedText(text : String) : String {