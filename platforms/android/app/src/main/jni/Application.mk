33afc48337646858387ea50df75c797bf41c696a
Ivan Vereschaga <vereschaga.ivan@gmail.com>
2026-03-26 10:13:05 +0200

    Android: Support 16KB page size; Update build tools; (#1592)
    * Android: Upgrade build tools; Force 16KB page size aligned binaries;
    
    * Add missing version parameters

--- platforms/android/app/src/main/jni/Application.mk
+++ platforms/android/app/src/main/jni/Application.mk
@@ -1,3 +1,6 @@
+# Enable flexible page size support for Android 15 compatibility with 16KB page sizes
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
+
 APP_STL := c++_shared
 APP_ABI := armeabi-v7a arm64-v8a
 APP_CPPFLAGS += -w -frtti -fexceptions -Ofast -std=c++11
